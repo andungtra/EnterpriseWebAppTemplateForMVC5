8d8784efa3cd75b6ced5a229b47c47305825c53d
Hüseyin Deniz KIVRAK <huseyindeniz_kivrak@hotmail.com>
2016-01-01 23:46:37 +0200

    Log domain added.

---
+++ source/EnterpriseAppTemplate4MVC5.Domain.Shared/Helper/IHelperSerializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseAppTemplate4MVC5.Domain.Shared.Helper
+{
+    public interface IHelperSerializer
+    {
+
+        string SerializeObjectWithXMLFormatter(object o);
+
+        byte[] DeSerializeObjectWithXMLFormatter(object o);
+
+    }
+
+}
---
+++ source/EnterpriseAppTemplate4MVC5.Domain.Shared/Service/IServiceDelete.cs
@@ -0,0 +1,19 @@
+﻿using EnterpriseAppTemplate4MVC5.Domain.Shared.ValueObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseAppTemplate4MVC5.Domain.Shared.Service
+{
+    public interface IServiceDelete<TPrimaryIdType, TType>
+    {
+
+        TType GetById(TPrimaryIdType id);
+
+        OperationResult Delete(TType entity);
+
+    }
+
+}
